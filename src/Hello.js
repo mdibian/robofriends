237d83047037a8652ba7efe2cd834d2ab8af2484
Maycon <maycondibianco@gmail.com>
2020-04-01 18:15:30 -0400

    Adds the Hello component.

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css';
+     
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>Welcome c:</p>
+      </div>
+    );
+  }
+}
+     
+export default Hello;
